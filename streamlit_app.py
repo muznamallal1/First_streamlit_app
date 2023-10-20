cfa3531f0d96add86681e5acc57ae7171751a424
muznamallal1 <148577689+muznamallal1@users.noreply.github.com>
2023-10-20 13:55:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,13 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket SMoothie')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Hard boiled Free Range eggs')
+
+
