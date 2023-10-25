e4a0ef70a4f8009ff5931c0f74f5137fc97cd051
muznamallal1 <148577689+muznamallal1@users.noreply.github.com>
2023-10-25 15:36:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,7 @@ streamlit.header("THe fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
 
+fruits_selected1 = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show1= my_fruit_list.loc[fruits_selected1]
+streamlit.dataframe(fruits_to_show1)
+
